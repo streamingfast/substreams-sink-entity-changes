bfff71ec249050bd3a816116c6319eaefd40916d
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-10-21 09:59:20 -0400

    removing build.rs and substreams entity rust file generated by build.rs

--- build.rs
+++
@@ -1,7 +0,0 @@
-use std::io::Result;
-
-fn main() -> Result<()> {
-    let mut prost_build = prost_build::Config::new();
-    prost_build.out_dir("./src/pb");
-    prost_build.compile_protos(&["entity.proto"], &["./proto/entity/v1/"])
-}
